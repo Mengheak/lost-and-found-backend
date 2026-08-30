9ab01fd003dd685f724e42b5c869ab4d21303b1e
Mengheak <mengheak088@gmail.com>
2026-08-30 20:42:56 +0700

    feat(controller): Added mockcontroller, testing ci/cd deployment

---
+++ src/main/java/com/group5/lostandfoundjava/controller/MockController.java
@@ -0,0 +1,13 @@
+package com.group5.lostandfoundjava.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MockController {
+    @GetMapping("/api/auth/mockapi")
+    public ResponseEntity<String> mockApi() {
+        return ResponseEntity.ok("mockapi tested");
+    }
+}
