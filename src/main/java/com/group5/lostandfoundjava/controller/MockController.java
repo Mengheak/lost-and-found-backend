056b515efde5eed53ab260d4b7578948e608bf3a
Mengheak <mengheak088@gmail.com>
2026-08-30 21:42:49 +0700

    modify mockcontroller

--- src/main/java/com/group5/lostandfoundjava/controller/MockController.java
+++ src/main/java/com/group5/lostandfoundjava/controller/MockController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MockController {
     @GetMapping("/api/auth/mockapi")
     public ResponseEntity<String> mockApi() {
-        return ResponseEntity.ok("mockapi tested");
+        return ResponseEntity.ok("MOCK CONTROLLER TESTED lerk ti2");
     }
 }
